d83482ff411fd49307c63708460a61db8e341a42
kenjiuno <h8mastre@gmail.com>
2016-07-20 13:36:57 +0900

    1.7.1

--- DiffBkRestore/Properties/AssemblyInfo.cs
+++ DiffBkRestore/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly:     AssemblyVersion("1.7")] // here
-[assembly: AssemblyFileVersion("1.7")] // here
+[assembly:     AssemblyVersion("1.7.1")] // here
+[assembly: AssemblyFileVersion("1.7.1")] // here
